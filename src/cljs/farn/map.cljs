03d618ea18067ea5aa92d287abf04b2a6ff48a94
Crispin Wellington <retrogradeorbit@gmail.com>
2015-01-27 22:21:18 +0800

    reference new PerlinMap namespace

--- src/cljs/farn/map.cljs
+++ src/cljs/farn/map.cljs
@@ -1,10 +1,11 @@
-(ns farn.map (
-              :require [cljs.core.async :refer [put! chan <! >! alts! timeout close!]]
+(ns farn.map
+  (:require
+   [cljs.core.async :refer [put! chan]]
               ))
 
 ; seed is 23 here
 ; TODO: check store for seed and generate a new one if none
 (defn perlin-map-generator []
   (let [c (chan)]
-    (js/map 23 1000 1000 (fn [t] (put! c t)))
+    (js/PerlinMap.map 23 1000 1000 (fn [t] (put! c t)))
     c))
